0cc17b1b50c6b2be48faa37e1a0abb816e04d386
lapiy <a.lapiy@gmail.com>
2025-04-11 16:25:16 +0300

    correct app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
-<<<<<<< HEAD
-
-@Module({
-  imports: [UsersModule],
-=======
 import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
   imports: [UsersModule, FirebaseModule],
->>>>>>> firebase
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
