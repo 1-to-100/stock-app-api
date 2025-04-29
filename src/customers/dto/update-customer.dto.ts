6b813126ed8298adbd784c9c7b3f74cf76daefef
odycceus <odycceus+git@gmail.com>
2025-04-29 17:43:23 +0300

    feat: add optional status field to update customer DTO with validation

--- src/customers/dto/update-customer.dto.ts
+++ src/customers/dto/update-customer.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateCustomerDto } from './create-customer.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { StatusList, StatusType } from '../../common/constants/status';
 
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {
+  @IsEnum(StatusList)
+  @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Status (optional). Default: inactive',
+    enum: StatusList,
+  })
+  status?: StatusType;
+}
