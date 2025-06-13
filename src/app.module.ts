883751ec496436ae4011147a5140f42bb71266ec
odycceus <odycceus+git@gmail.com>
2025-06-13 12:44:39 +0300

    build: remove local firebase module from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { UsersModule } from '@/users/users.module';
 import { PrismaService } from '@/common/prisma/prisma.service';
-import { FirebaseModule as LocalFirebaseModule } from '@/firebase/firebase.module';
 import { AuthModule } from '@/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { SystemModulesModule } from '@/system-modules/system-modules.module';
@@ -20,7 +19,6 @@ import { NotificationsModule } from '@/notifications/notifications.module';
 @Module({
   imports: [
     UsersModule,
-    LocalFirebaseModule,
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     SystemModulesModule,
--- src/firebase/firebase.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FirebaseController } from '@/firebase/firebase.controller';
-
-@Module({
-  controllers: [FirebaseController],
-})
-export class FirebaseModule {}
